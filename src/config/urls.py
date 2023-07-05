8a7da394033637cb6272fb10b382bc84468d3251
vadim hillel <vadimfootball1304@gmail.com>
2023-07-05 14:46:27 +0200

    🗃️ Src

---
+++ src/config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("src.users.creation")),
+    path("", include("trash.pokemons")),
+]
