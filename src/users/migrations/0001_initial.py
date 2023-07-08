133e1df59d9301007cdc7011261aa1ac3e7f8ef5
vadim hillel <vadimfootball1304@gmail.com>
2023-07-08 17:12:34 +0200

    Updated

--- src/users/migrations/0001_initial.py
+++ src/users/migrations/0001_initial.py
@@ -27,7 +27,8 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False)),
                 ('is_active', models.BooleanField(default=True)),
                 ('role', models.PositiveSmallIntegerField()),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', 
+                                                  related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
             options={
