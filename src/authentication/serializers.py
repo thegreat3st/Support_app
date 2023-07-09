b99a92ebd49b299b678bb1935120a66d02f4aa52
vadim hillel <vadimfootball1304@gmail.com>
2023-07-09 10:27:03 +0200

    Updated

--- src/authentication/serializers.py
+++ src/authentication/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer): # LoginRequestSerializer
 
     @classmethod
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
 
-        # Add custom claims
         token['email'] = user.email
         return token
     
