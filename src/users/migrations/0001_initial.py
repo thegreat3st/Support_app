af45d854a7aa3f05fc160befa015b1fac95dde7d
vadim hillel <vadimfootball1304@gmail.com>
2023-07-11 11:03:51 +0200

    #noqa update

--- src/users/migrations/0001_initial.py
+++ src/users/migrations/0001_initial.py
@@ -16,19 +16,20 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name='User',
+            # flake8: noqa
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),# noqa
-                ('password', models.CharField(max_length=128, verbose_name='password')),# noqa
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),# noqa
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),# noqa
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
                 ('email', models.EmailField(max_length=150, unique=True)),
                 ('first_name', models.CharField(max_length=100, null=True)),
                 ('last_name', models.CharField(max_length=100, null=True)),
                 ('is_staff', models.BooleanField(default=False)),
                 ('is_active', models.BooleanField(default=True)),
                 ('role', models.PositiveSmallIntegerField()),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),# noqa
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),# noqa
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
             options={
                 'db_table': 'users',
@@ -36,6 +37,7 @@ class Migration(migrations.Migration):
         ),
         migrations.CreateModel(
             name='Request',
+            # flake8: noqa
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),# noqa
                 ('title', models.CharField(max_length=100)),
@@ -49,13 +51,14 @@ class Migration(migrations.Migration):
                 'db_table': 'requests',# noqa
             },
         ),
-        migrations.CreateModel( # noqa
-            name='Message',# noqa
-            fields=[# noqa
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),# noqa
+        migrations.CreateModel( 
+            name='Message',
+            # flake8: noqa
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.TextField()),
-                ('request', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, related_name='messages', to='users.request')),# noqa
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, related_name='messages', to=settings.AUTH_USER_MODEL)),# noqa
+                ('request', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, related_name='messages', to='users.request')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, related_name='messages', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'messages',
