afc32a655ba244add6b17c5134280749c3fc161e
vadim hillel <vadimfootball1304@gmail.com>
2023-07-21 11:14:16 +0200

    task 19 Updated

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,21 +1,33 @@
 from django.contrib import admin
 from django.urls import path, include
 from src.authentication.routers import CustomReadOnlyRouter
-from src.authentication.views import UserViewSet# , MyObtainTokenPairView
-from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenRefreshView
+from src.authentication.views import UserViewSet # MyObtainTokenPairView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = CustomReadOnlyRouter()
-router.register(r'user', UserViewSet, basename='User')
+router.register('', UserViewSet, basename='user')
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Support API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", include("src.users.api")),
-    path('', include('trash.pokemons')),
-    # path('token/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    # path('', include('trash.pokemons')), POKEMONS LINK
     path('auth/', include('src.authentication.urls')),
     path('tickets/', include('src.tickets.urls')),
     path('', include(router.urls)),
